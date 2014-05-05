ef5d6d137f87ec2339c6d4a5071a35e3bbd404de
Gabriel Zea <zea@randomlab.net>
2014-05-05 15:10:27 -0500

    comentarios

--- data/registro.js
+++ data/registro.js
@@ -3,10 +3,42 @@ self.port.on("replacePage", function(msg){
 
   console.log("-------------- FF addon");
 
-  $(document).keypress(function (e){
+  // donde guardar el consolidado de caracteres
+  var finalString = '';
+
+  // revisa en todo el documento cuando se presiona una tecla
+  $(document).keypress(function (e){  
+    // el codigo de la tecla
     var code = e.which;
+    // el caracter decoficado
     var character = String.fromCharCode(code);
-    
     console.info("presionaron: "+ code + " == carater " + character);
+
+    // suma el nuevo caracter 
+    finalString += character;
+
+    // esta pendiente de la tecla enter
+    if (code === 13){
+      console.info("Presionado enter");
+      console.info('suma de caracteres: ', finalString);
+      //llama la funcion que enviara la info
+      postData(finalString);
+    }
   }); 
+
+  // esta pendiente de los botones
+  $('input[type=submit]').click(function (e){
+    e.preventDefault();
+
+    console.info("Presionado boton");
+    console.info('suma de caracteres: ', finalString);
+    // llama la funcion que enviara la info
+    postData(finalString);
+  });
 });
+
+// Aca tiene que definirse a donde se va la informcion
+function postData(data){
+  console.info("datos a enviar: ", data);
+}
+
